5e72de11e66f491fa3dc641c099f4e1404fff203
Tobigod1234 <152081331+Tobigod1234@users.noreply.github.com>
2023-12-30 17:18:10 +0530

    Update myDb.py

--- bot/myDb.py
+++ bot/myDb.py
@@ -5,7 +5,7 @@ from threading import Lock
 
 class Redis:
     def __init__(self, ttl=60) -> None:
-        self.client = redis.Redis(
+        self.redis_pool = redis.ConnectionPool(
             host=Config.REDIS_HOST,
             port=Config.REDIS_PORT,
             password=Config.REDIS_PASS,
@@ -14,32 +14,39 @@ class Redis:
         self.lock = Lock()
 
     def conn(self):
-        self.client.ping()
+        redis_client = redis.StrictRedis(connection_pool=self.redis_pool)
+        redis_client.ping()
 
     def gen_token(self, user_id: int) -> str:
         token = f"{str(uuid4())[0:6]}"
         with self.lock:
-            self.client.set(user_id, token, ex=self.ttl)
-        print(token)
+            redis_client = redis.StrictRedis(connection_pool=self.redis_pool)
+            redis_client.set(user_id, token, ex=self.ttl)
         return token
 
     def check_access(self, user_id: int) -> bool:
-        return self.client.get(user_id) is not None
+        with self.lock:
+            redis_client = redis.StrictRedis(connection_pool=self.redis_pool)
+            return redis_client.get(user_id) is not None
 
     def got_key(self, user_id):
         try:
-            return self.client.get(f"acc^{user_id}").decode('utf-8') == '1'
+            with self.lock:
+                redis_client = redis.StrictRedis(connection_pool=self.redis_pool)
+                return redis_client.get(f"acc^{user_id}").decode('utf-8') == '1'
         except:
             return False
 
     def accessed(self, user_id: int, key: str) -> bool:
         try:
             with self.lock:
-                if self.client.get(user_id).decode('utf-8') == key:
-                    self.client.set(f"acc^{user_id}", 1, self.ttl)
+                redis_client = redis.StrictRedis(connection_pool=self.redis_pool)
+                if redis_client.get(user_id).decode('utf-8') == key:
+                    redis_client.set(f"acc^{user_id}", 1, self.ttl)
                     return True
                 return False
         except:
             return False
 
 myDb = Redis(Config.TIMEOUT)
+myDb.conn()  
