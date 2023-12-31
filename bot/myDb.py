2b78fb4abad5b92f3aa5f5d1abd1bff0c93cee2f
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2023-12-31 18:16:29 +0530

    Update myDb.py

--- bot/myDb.py
+++ bot/myDb.py
@@ -1,50 +1,41 @@
-import redis
+from pymongo import MongoClient
 from bot.config import Config
 from uuid import uuid4
 
-class Redis:
+class MongoDB:
 
- def init(self, ttl=60) -> None:
-  self.client = redis.Redis(
-   host=Config.REDIS_HOST,
-   port=Config.REDIS_PORT,
-   password=Config.REDIS_PASS,
-  )
+    def __init__(self, ttl=60) -> None:
+        self.client = MongoClient(Config.MONGO_URI)
+        self.db = self.client[Config.MONGO_DB]
+        self.ttl = ttl
 
-  self.ttl = ttl
+    def conn(self):
+        self.client.server_info()
 
- def conn(self):
-  self.client.ping()
+    def gen_token(self, user_id: int) -> str:
+        token = f"{str(uuid4())[0:6]}"
+        self.db.tokens.insert_one({"_id": user_id, "token": token})
+        print(token)
+        return token
 
- def gen_token(self, user_id: int) -> str:
+    def check_access(self, user_id: int) -> bool:
+        return self.db.tokens.find_one({"_id": user_id}) is not None
 
-  token = f"{str(uuid4())[0:6]}"
-  self.client.set(user_id, token, ex=self.ttl)
+    def got_key(self, user_id):
+        try:
+            return self.db.access.find_one({"_id": f"acc^{user_id}"})['value'] == '1'
+        except:
+            return False
 
-  print(token)
-
-  return token
-
- def check_access(self, user_id: int) -> bool:
-  return self.client.get(user_id) != None
-
-
- def got_key(self, user_id):
-  try:
-   return self.client.get(f"acc^{user_id}").decode('utf-8') == '1'
-  except:
-   return False
-
- def accessed(self, user_id: int, key: str) -> bool:
-  try:
-
-   if self.client.get(user_id).decode('utf-8') == key:
-    self.client.set(f"acc^{user_id}", 1, self.ttl)
-    return True
-
-   return False
-  except: return False
+    def accessed(self, user_id: int, key: str) -> bool:
+        try:
+            document = self.db.tokens.find_one({"_id": user_id, "token": key})
+            if document:
+                self.db.access.update_one({"_id": f"acc^{user_id}"}, {"$set": {"value": '1'}}, upsert=True)
+                return True
+            return False
+        except:
+            return False
 
 
-
-myDb = Redis(Config.TIMEOUT)
+myDb = MongoDB(Config.TIMEOUT)
