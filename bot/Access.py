3068f1af63fe188d97fe8adc9ce88cafed9781b9
Tobigod1234 <152081331+Tobigod1234@users.noreply.github.com>
2023-12-31 18:34:07 +0530

    Create Access.py

---
+++ bot/Access.py
@@ -0,0 +1,33 @@
+from bot.myDb import myDb
+from bot import TGBot, logger
+from bot.url import short_me
+from bot.config import Config
+
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client
+
+def access(func):
+ async def wrapper(client: Client, msg: Message):
+  try:
+   if msg.from_user.id in Config.AUTH_USERS:
+    print("IS ADMIN")
+    return await func(client, msg)
+
+   token_live = myDb.check_access(msg.from_user.id)
+
+   if not token_live:
+
+    token = myDb.gen_token(msg.from_user.id)
+    bot_ = await TGBot.get_me()
+
+
+    btn = [[InlineKeyboardButton("Gen Token", url=short_me(f"https://telegram.me/{bot_.username}?start={token}"))]]
+
+    await msg.reply(
+     text=f"You need to generate Token to use me {msg.from_user.mention} .\n\n",
+     reply_markup=InlineKeyboardMarkup(btn),
+    )
+
+    myDb.client.set(f"acc^{msg.from_user.id}", 0)
+
+    return
