8d351939f92e92ee029988b4d40da77cd7e6b66b
Tobigod1234 <152081331+Tobigod1234@users.noreply.github.com>
2023-12-31 18:39:27 +0530

    Update Access.py

--- bot/Access.py
+++ bot/Access.py
@@ -7,27 +7,26 @@ from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram import Client
 
 def access(func):
- async def wrapper(client: Client, msg: Message):
-  try:
-   if msg.from_user.id in Config.AUTH_USERS:
-    print("IS ADMIN")
-    return await func(client, msg)
+    async def wrapper(client: Client, msg: Message):
+        try:
+            if msg.from_user.id in Config.AUTH_USERS:
+                print("IS ADMIN")
+                return await func(client, msg)
 
-   token_live = myDb.check_access(msg.from_user.id)
+            token_live = myDb.check_access(msg.from_user.id)
 
-   if not token_live:
+            if not token_live:
+                token = myDb.gen_token(msg.from_user.id)
+                bot_ = await TGBot.get_me()
+                btn = [[InlineKeyboardButton("Gen Token", url=short_me(f"https://telegram.me/{bot_.username}?start={token}"))]]
+                await msg.reply(
+                    text=f"You need to generate Token to use me {msg.from_user.mention}.\n\n",
+                    reply_markup=InlineKeyboardMarkup(btn),
+                )
+                myDb.client.set(f"acc^{msg.from_user.id}", 0)
+                return
 
-    token = myDb.gen_token(msg.from_user.id)
-    bot_ = await TGBot.get_me()
+        except Exception as e:
+            logger.error(f"Error in access function: {e}")
 
-
-    btn = [[InlineKeyboardButton("Gen Token", url=short_me(f"https://telegram.me/{bot_.username}?start={token}"))]]
-
-    await msg.reply(
-     text=f"You need to generate Token to use me {msg.from_user.mention} .\n\n",
-     reply_markup=InlineKeyboardMarkup(btn),
-    )
-
-    myDb.client.set(f"acc^{msg.from_user.id}", 0)
-
-    return
+    return wrapper
